3938cc2ff52a265e31f3ae06aa57a3cd83f2eb53
olebuhoe <olebuhoe@yandex.ru>
2022-10-13 15:16:42 +0300

    add Работа со случайными числами

--- exam03_sum2num/Program.cs
+++ exam03_sum2num/Program.cs
@@ -1,3 +1,5 @@
-﻿int num1 = 7;
-int num2 = 2;
+﻿int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num1);
+Console.WriteLine(num2);
 Console.WriteLine(num1 + num2);
--- exam04_division/Program.cs
+++ exam04_division/Program.cs
@@ -1,4 +1,3 @@
 ﻿double num1 = 7;
 double num2 = 2;
-// float res = num1 / num2;
 Console.WriteLine(num1 / num2);
