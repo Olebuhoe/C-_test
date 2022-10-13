d08bc58cea19d64ef2655f02d64fdbdd4965a133
olebuhoe <olebuhoe@yandex.ru>
2022-10-13 17:03:34 +0300

    Ветвление

---
+++ exam05_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ебать, это же МАХА!!!");
+}
+else
+{
+    Console.Write("Ну, и хули надо, ");
+    Console.Write(username);
+    Console.Write("?");
+}
