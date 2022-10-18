caf458810d3a07c28bc7e6d92823102dc9a34662
olebuhoe <olebuhoe@yandex.ru>
2022-10-18 18:32:23 +0300

    Объявление массива, заполнение, печать эл-тов, поиск номера эл-та массива

---
+++ exam11_func_array/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] arr = new int[10];
+//Console.WriteLine(array);
+
+void FillArray(int[] collection)
+{
+    int idx = 0;
+    while (idx < collection.Length)
+    {
+        collection[idx] = new Random().Next(1, 10);
+        idx++;
+    }
+}
+
+void PrintArrElem(int[] array)
+{
+    int idx = 0;
+    while (idx < array.Length)
+    {
+        Console.Write(array[idx] + " ");
+        idx++;
+    }
+}
+
+int IndexOf(int[] array, int find)
+{
+    int idx = 0;
+    int pos = -100;
+    while (idx < array.Length)
+    {
+        if (array[idx] == find)
+        {
+            pos = idx;
+            break;
+        }
+        idx++;
+    }
+    return pos;
+}
+
+FillArray(arr);
+PrintArrElem(arr);
+Console.WriteLine();
+Console.WriteLine(IndexOf(arr, 3));
