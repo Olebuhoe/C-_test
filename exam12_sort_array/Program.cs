1e32b36bb52d0a96f232df972c24c5b1af7bc46d
olebuhoe <olebuhoe@yandex.ru>
2022-11-01 17:01:38 +0300

    Сортировка массива: цикл в цикле

---
+++ exam12_sort_array/Program.cs
@@ -0,0 +1,58 @@
+﻿int[] arr = {7, 8, 5, 1, 4, 4, 2, 11};
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(arr);
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPos = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPos])
+            {
+                minPos = j;
+            }
+        }
+
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPos = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPos])
+            {
+                maxPos = j;
+            }
+        }
+
+        int temp = array[i];
+        array[i] = array[maxPos];
+        array[maxPos] = temp;
+    }
+}
+
+SelectionSort(arr);
+Console.WriteLine();
+PrintArray(arr);
+Console.WriteLine();
+SelectionSortMax(arr);
+PrintArray(arr);
